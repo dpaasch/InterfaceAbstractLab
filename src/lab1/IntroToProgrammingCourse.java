f83f69395a6700c1a8072a19861fecac009c0a41
dpaasch@my.wctc.edu <>
2013-01-31 20:20:49 -0600

    Update #9 - Cleaned up the IntroToProgrammingCourse class to simplify it, moving all comments to the end of the code.

--- src/lab1/IntroToProgrammingCourse.java
+++ src/lab1/IntroToProgrammingCourse.java
@@ -10,53 +10,64 @@ import javax.swing.JOptionPane;
  * @version 1.00
  */
 public class IntroToProgrammingCourse extends ProgrammingDegree {
-    // Moving these properties to the superclass, as they are common among each
-    // of the sub-classes.
-    // private String courseName;
-    // private String courseNumber;
-    // private double credits;
 
     public IntroToProgrammingCourse(String courseName, String courseNumber) {
-        this.setCourseName(courseName);
-        this.setCourseNumber(courseNumber);
+        super(courseName, courseNumber);
     }
 
-//    // Inherited methods, which are no longer needed here.
-//    public String getCourseName() {
-//        return courseName;
-//    }
-    
-//    public final void setCourseName(String courseName) {
-//        if (courseName == null || courseName.length() == 0) {
-//            JOptionPane.showMessageDialog(null,
-//                    "Error: courseName cannot be null of empty string");
-//            System.exit(0);
-//        }
-//        this.courseName = courseName;
-//    }
-    
-//    public final void setCourseNumber(String courseNumber) {
-//        if (courseNumber == null || courseNumber.length() == 0) {
-//            JOptionPane.showMessageDialog(null,
-//                    "Error: courseNumber cannot be null of empty string");
-//            System.exit(0);
-//        }
-//        this.courseNumber = courseNumber;
-//    }
-    
-//    public String getCourseNumber() {
-//        return courseNumber;
-//    }
-//    public double getCredits() {
-//        return credits;
-//    }
-    
-//    public void setCredits(double credits) {
-//        if (credits < 0.5 || credits > 4.0) {
-//            JOptionPane.showMessageDialog(null,
-//                    "Error: credits must be in the range 0.5 to 4.0");
-//            System.exit(0);
-//        }
-//        this.credits = credits;
-//    }
+    public void setCredits(double credits) {
+        if (credits < 0.5 || credits > 4.0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        // Changing this to the setCredits method: this.credits = credits;
+        setCredits(credits);
+    } 
 }
+/**
+ * Have made the following changes within this sub-class to clean-up/simplify
+ * the code:
+ * 1) Moved these properties to the superclass, as they are common among each of 
+ * the sub-classes.
+ * private String courseName;
+ * private String courseNumber;
+ * private double credits;
+ * 
+ * 2) As these private properties are now in the superclass, I have decided to
+ * call the superclass constructor to populate this constructor.  Therefore, 
+ * these statements are no longer required.
+ * this.setCourseName(courseName);
+ * this.setCourseNumber(courseNumber);
+ * 
+ * 3) These are now inherited methods, so they are no longer required here.
+ * public String getCourseName() {
+ *  return courseName;
+ * }
+ * 
+ * public final void setCourseName(String courseName) {
+ *  if (courseName == null || courseName.length() == 0) {
+ *      JOptionPane.showMessageDialog(null,
+ *          "Error: courseName cannot be null of empty string");
+ *      System.exit(0);
+ *      }
+ *      this.courseName = courseName;
+ * }
+ *
+ * public final void setCourseNumber(String courseNumber) {
+ *  if (courseNumber == null || courseNumber.length() == 0) {
+ *      JOptionPane.showMessageDialog(null,
+ *          "Error: courseNumber cannot be null of empty string");
+ *      System.exit(0);
+ *      }
+ *      this.courseNumber = courseNumber;
+ * }
+ * 
+ * public String getCourseNumber() {
+ *  return courseNumber;
+ * }
+ * 
+ * public double getCredits() {
+ *  return credits;
+ * }
+ */
