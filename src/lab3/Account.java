226a39ea8f15277c385320b6b307af349b41a906
dpaasch@my.wctc.edu <>
2013-02-03 16:12:27 -0600

    Update #17 - Created the classes for Lab #3, still working through the logistics (which will be the interface, abstract, concrete, etc.).

---
+++ src/lab3/Account.java
@@ -0,0 +1,15 @@
+package lab3;
+
+/**
+ * Which super classes should be concrete vs. abstract vs. Interface?  Justify 
+ * your choices by adding comments to each class.
+ * This super class contains
+ * @author Dawn Bykowski
+ */
+public interface Account {
+    
+   public void withdrawl(float amount);
+   public void deposit(float amount);
+   public int getAccountID();
+    
+}
---
+++ src/lab3/BankAccount.java
@@ -0,0 +1,18 @@
+
+package lab3;
+
+public class BankAccount implements Account {
+
+    public void withdrawl(float amount) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void deposit(float amount) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int getAccountID() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
